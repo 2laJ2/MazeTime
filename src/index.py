2b4537cc3a764ae0137ebfbd2e99f09af1ce7884
2laJ2 <tuulajakobsson@hotmail.com>
2022-03-18 23:17:37 +0200

    add test

--- src/index.py
+++ src/index.py
@@ -1,15 +1,17 @@
 from maze import Maze
 
+
 def main():
-    
+
     def letsBegin():
         print("Let's have some fun with mazes!")
-       
+
     letsBegin()
 
     maze1 = Maze(1)
 
     print(f"{maze1.viesti()}")
+
 
 if __name__ == "__main__":
     main()
--- src/tests/maze_test.py
+++ src/tests/maze_test.py
@@ -2,9 +2,14 @@ import unittest
 from optparse import make_option
 from maze import Maze
 
+
 class TestMain(unittest.TestCase):
     def setUp(self):
         self.maze = Maze(1)
 
     def test_ohjelma_luo_labyrintin_joka_antaa_tervehdyksen(self):
         self.assertEqual(self.maze.viesti(), "Hello, I'm a maze.")
+
+    def test_labyrintti_palauttaa_oikean_nimen(self):
+        self.assertEqual(self.maze.get_nimi(), 1)
+        
